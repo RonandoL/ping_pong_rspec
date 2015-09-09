e380014c5afa26a21b79845c42b9b7022dfa78ab
Ronando Long <ronando.long@gmail.com>
2015-09-09 12:01:19 -0700

    final spec failed, prior to coding.

--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -11,8 +11,12 @@ describe("Fixnum#ping_pong") do
   #   expect((16).ping_pong()).to(eq(["ping-pong"]))
   # end   # # this passed, but then became depricated
 
-  it ("Creates array with ping-pong for multiples of (3*5) and 'ping' for multiples of 3") do
+  it("Creates array with ping-pong for multiples of (3*5) and 'ping' for multiples of 3") do
     expect((16).ping_pong()).to(eq([1, 2, "ping", 4, 5, "ping", 7, 8, "ping", 10, 11, "ping", 13, 14, "ping-pong", 16]))
+  end
+
+  it("Creates array with 'ping', 'pong' and 'ping-pong' for all multiples of given variables") do
+    expect((16).ping_pong()).to(eq([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "ping-pong", 16]))
   end
 
 end
